4af759e2dae0c6fd17daea8b0e570c8caa08af89
Tim-Dream <13866131255@163.com>
2024-04-20 15:02:59 +0800

    企业微信小程序和h5页面开发

---
+++ qywx-mini-h5/src/test/java/com/dream/qywxminih5/QywxMiniH5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.qywxminih5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QywxMiniH5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

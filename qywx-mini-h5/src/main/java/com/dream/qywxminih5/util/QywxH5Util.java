036c4b5040940ee5f3c51f398fe056c71f83f32a
HuangJie <13866131255@163.com>
2024-04-29 17:31:01 +0800

    Update QywxH5Util.java

--- qywx-mini-h5/src/main/java/com/dream/qywxminih5/util/QywxH5Util.java
+++ qywx-mini-h5/src/main/java/com/dream/qywxminih5/util/QywxH5Util.java
@@ -1,12 +1,17 @@
 package com.dream.qywxminih5.util;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.security.MessageDigest;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 @Slf4j
 @Component
@@ -62,6 +67,51 @@ public class QywxH5Util {
             map = JSONObject.parseObject(sessionRst, Map.class);
         }
         return map;
+    }
+
+    /**
+     * 获取JS_SDK 鉴权所需的token
+     * @return
+     */
+    public String getJsapiTicket(){
+        try {
+            String sessionUrl = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=" + getQywxToken();
+            String sessionRst = HttpUtil.get(sessionUrl);
+            Map result = JSONObject.parseObject(sessionRst, Map.class);
+            if (String.valueOf(result.get("errcode")).equals("40014")) {
+//              TODO 鉴权失败，清除Redis中缓存的 token
+                sessionUrl = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=" + getQywxToken();
+                sessionRst = HttpUtil.get(sessionUrl);
+                result = JSONObject.parseObject(sessionRst, Map.class);
+            }
+            return (String) result.get("ticket");
+        } catch (Exception e) {
+            throw new RuntimeException("企业微信获取Ticket失败");
+        }
+    }
+
+    /**
+     * H5 页面想要调用企业微信的一些接口，需要对调用页面的URL进行鉴权
+     * @param url 需要调用SDK的页面的URL
+     * @return 所需参数
+     */
+    public SortedMap<String, String> obtainConfigParam(String url) {
+        String ticket = this.getJsapiTicket();
+        String nonceStr = RandomUtil.randomString(10);
+        String timeStamp = Long.toString(System.currentTimeMillis() / 1000);
+
+        SortedMap<String, String> params = new TreeMap<String, String>();
+        params.put("noncestr", nonceStr);
+        params.put("jsapi_ticket", ticket);
+        params.put("timestamp", timeStamp);
+        params.put("url", url);
+        String signature = sortSignByASCII(params);
+        signature = sha1Digest(signature);
+
+        params.put("signature", signature);
+        params.put("appId", corpid);
+
+        return params;
     }
 
     /**
@@ -130,4 +180,62 @@ public class QywxH5Util {
         HttpUtil.post(realUrl, sb);
     }
 
+
+
+     /**
+     * <h2>对所有待签名参数按照字段名的ASCII 码从小到大排序</h2>
+     *
+     * @return java.lang.String
+     * @Author nicky
+     * @Date 2021/04/25 20:22
+     * @Param [params]
+     */
+     private static String sortSignByASCII(SortedMap<String, String> parameters) {
+        // 以k1=v1&k2=v2...方式拼接参数
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, String> s : parameters.entrySet()) {
+            String k = s.getKey();
+            String v = s.getValue();
+            // 过滤空值
+            if (StringUtils.isEmpty(v)) {
+                continue;
+            }
+            builder.append(k).append("=").append(v).append("&");
+        }
+        if (!parameters.isEmpty()) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * sha1加密 <br>
+     *
+     * @return java.lang.String
+     * @Author nicky
+     * @Date 2021/04/26 10:22
+     * @Param [str]
+     */
+    private static String sha1Digest(String str) {
+        try {
+            // SHA1签名生成
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.update(str.getBytes());
+            byte[] digest = md.digest();
+
+            StringBuffer hexstr = new StringBuffer();
+            String shaHex = "";
+            for (int i = 0; i < digest.length; i++) {
+                shaHex = Integer.toHexString(digest[i] & 0xFF);
+                if (shaHex.length() < 2) {
+                    hexstr.append(0);
+                }
+                hexstr.append(shaHex);
+            }
+            return hexstr.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
